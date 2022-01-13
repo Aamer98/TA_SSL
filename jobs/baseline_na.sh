ed6fc6425d08fa0a2d075205cb08ce00950fc3db
unknown <ar.aamer@gmail.com>
2022-01-13 19:32:53 +0400

    a

---
+++ jobs/baseline_na.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#SBATCH --mail-user=ar.aamer@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=baseline_na
+#SBATCH --output=%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=127000M
+#SBATCH --time=2-00:00
+#SBATCH --account=rrg-ebrahimi
+
+nvidia-smi
+
+source ~/my_env9/bin/activate
+
+echo "------------------------------------< Data preparation>----------------------------------"
+echo "Copying the source code"
+date +"%T"
+cd $SLURM_TMPDIR
+cp -r ~/scratch/TA_SSL .
+
+echo "Copying the datasets"
+date +"%T"
+cp -r ~/scratch/CD-FSL_Datasets .
+
+echo "creating data directories"
+date +"%T"
+cd TA_SSL
+cd data
+unzip -q $SLURM_TMPDIR/CD-FSL_Datasets/miniImagenet.zip
+
+echo "----------------------------------< End of data preparation>--------------------------------"
+date +"%T"
+echo "--------------------------------------------------------------------------------------------"
+
+echo "---------------------------------------<Run the program>------------------------------------"
+date +"%T"
+cd $SLURM_TMPDIR
+
+cd TA_SSL
+
+python baseline_na.py --dir ./logs/baseline_na --bsize 128 --epochs 1000 --model resnet10 &
+
+wait
+
+echo "-----------------------------------<End of run the program>---------------------------------"
+date +"%T"
+echo "--------------------------------------<backup the result>-----------------------------------"
+date +"%T"
+cd $SLURM_TMPDIR
+cp -r $SLURM_TMPDIR/TA_SSL/logs/baseline_na/ ~/scratch/TA_SSL/logs/
